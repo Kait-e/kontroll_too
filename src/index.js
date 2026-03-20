f6c78865707ac09f244a745c866100b023abdc89
Kait-e <Kait-evander.uusimaa@voco.ee>
2026-03-20 11:46:31 +0200

    ülesanne_1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Header from './components/Header';
+import Meals from './components/Meals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Meals />
   </React.StrictMode>
 );
